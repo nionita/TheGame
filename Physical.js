2cf601662a4818bc1df89bb94ec0afb45cb4f853
Nicu Ionita <nicu@ionita.at>
2012-12-08 22:22:40 +0100

    3 physical object types + keyboard control

--- Physical.js
+++ Physical.js
@@ -65,14 +65,25 @@ function Ball(r, pos, vel, col) {
 	this.tick = function(mi, ma) { this.body.tick(mi, ma); }
 }
 
-function Quad() {
+function Sonne(r, pos, vel, per) {
+	var pi2 = PI2 / 8;
+	this.body = new Body(r, pos, vel);
+	this.color = "rgb(110, 10, 20)";	// aspect
+	this.per = per || 20;	// rotation time
+	this.roll = 0
 	this.draw = function(ctx) {
 		ctx.save();
-		ctx.translate(ball.x, ball.y);
-		var r = ball.r;
-		ctx.fillStyle = ball.color;
+		ctx.fillStyle = this.color;
 		ctx.strokeStyle = "#D0D0F4";
-		ctx.lineWidth = 2;
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		var pos = this.body.pos;
+		ctx.translate(pos.x, pos.y);
+		ctx.rotate(PI2 * this.roll / this.per);
+		var r = this.body.rad;
+		ctx.fillStyle = this.color;
+		// ctx.strokeStyle = this.color"#D0D0F4";
+		ctx.lineWidth = 1;
 		ctx.beginPath();
 		ctx.moveTo(r, r);
 		ctx.lineTo(-r, r);
@@ -80,7 +91,82 @@ function Quad() {
 		ctx.lineTo(r, -r);
 		ctx.closePath();
 		ctx.fill();
+		// ctx.stroke();
+		ctx.rotate(pi2);	// the same rotated 45 degrees
+		ctx.beginPath();
+		ctx.moveTo(r, r);
+		ctx.lineTo(-r, r);
+		ctx.lineTo(-r, -r);
+		ctx.lineTo(r, -r);
+		ctx.closePath();
+		ctx.fill();
+		ctx.restore();
+	};
+	this.tick = function(mi, ma) {
+		this.body.tick(mi, ma);
+		this.roll = this.roll + 1
+		if (this.roll > this.per) { this.roll = 0 }
+	}
+}
+
+function Ship(pos) {
+	var maxgas = 100;	// maximum time we can accelerate
+	var pi2 = Math.PI / 2;
+	var power = 0.03;	// absolute acceleration
+	var ang = pi2 / 20;	// one degree direction control
+	this.body = new Body(5, pos);
+	this.dir = 0;		// direction (in radians)
+	this.color = "rgb(210, 200, 150)";	// aspect
+	this.gas = maxgas;
+	this.control_acc = false;	// is just accelerating?
+	this.control_left = false;		// direction control
+	this.control_right = false;
+	this.draw = function(ctx) {
+		var pos = this.body.pos;
+		var r = this.body.rad * 0.7;
+		var r2 = r / 2;
+		ctx.save();
+		ctx.translate(pos.x, pos.y);
+		ctx.rotate(this.dir);
+		ctx.fillStyle = this.color;
+		if (this.control_acc) {
+			ctx.strokeStyle = "#FFD0F0";
+		} else if (this.control_left || this.control_right) {
+			ctx.strokeStyle = "#80FFF0";
+		} else {
+			ctx.strokeStyle = "#D0D0F4";
+		}
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		ctx.moveTo(r, 0);
+		ctx.lineTo(-r2, r2);
+		ctx.lineTo(0, 0);
+		ctx.lineTo(-r2, -r2);
+		ctx.closePath();
+		ctx.fill();
 		ctx.stroke();
 		ctx.restore();
+	};
+	this.tick = function(mi, ma) {
+		var vel0 = this.body.vel;
+		this.body.tick(mi, ma);
+		var vel1 = this.body.vel;
+		if (vel1.x * vel1.x < 0) { this.dir = Math.PI - this.dir; }
+		if (vel1.y * vel1.y < 0) { this.dir = pi2     + this.dir; }
+		if (this.control_left) {
+			this.dir -= ang;
+		}
+		if (this.control_right) {
+			this.dir += ang;
+		}
+		if (this.control_acc && this.gas > 0) {
+			this.gas -= 1;
+			var ax = power * Math.cos(this.dir);
+			var ay = power * Math.sin(this.dir);
+			var myacc = new Vector(ax, ay);
+			vel1.addv(myacc);
+		} else if (this.gas < maxgas) {
+			this.gas += 1;
+		}
 	}
 }
--- helper.js
+++ helper.js
@@ -4,6 +4,15 @@ var $ = function(id) {
 		return document.getElementById(id);
 };
 
+var attachEvent = function(obj, event, callback) {
+	if (obj.addEventListener)
+		obj.addEventListener(event, callback, false);
+	else if (obj.attachEvent)  
+		obj.attachEvent('on' + event, callback);  
+	else
+		obj['on' + event] = callback;
+};
+
 var Game = (function() {
 	var TimerInterval = 40;	// in ms
 	var canvas = null;
@@ -12,6 +21,8 @@ var Game = (function() {
 	var height = 0;
 	var running = false;
 	var ball = null;
+	var sonne1 = null, sonne2 = null;
+	var ship = null;
 	var timer = null;
 	var pmin = null;
 	var pmax = null;
@@ -24,12 +35,15 @@ var Game = (function() {
         ctx.fillStyle = bkgGrad;
         ctx.fillRect(0, 0, width, height);
 
-		ball.draw(ctx);
+		ball.draw(ctx); sonne1.draw(ctx); sonne2.draw(ctx); ship.draw(ctx);
 	}
 
 	function tick() {
 		if (running) {
 			ball.tick(pmin, pmax);
+			sonne1.tick(pmin, pmax);
+			sonne2.tick(pmin, pmax);
+			ship.tick(pmin, pmax);
 			updateCanvas();
 		}
 	}
@@ -39,7 +53,49 @@ var Game = (function() {
 		var vel = new Vector(3, 2);
 		ball = new Ball(5, pos, vel, "rgb(88, 24, 10)");
 	}
-	
+
+	function initSonne() {
+		var pos = new Vector(450, 5);
+		var vel = new Vector(2, 5);
+		sonne1 = new Sonne(10, pos, vel, 50);
+		pos = new Vector(650, 55);
+		vel = new Vector(1, 4);
+		sonne2 = new Sonne(12, pos, vel, 30);
+	}
+
+	function initShip() {
+		var pos = new Vector(45, 545);
+		ship = new Ship(pos);
+	}
+
+	// For the keys we have following values:
+	// 38 (up)    - accelerate
+	// 37 (left)  - turn left
+	// 39 (right) - turn right
+	// all the rest we ignore
+	function onkeydown(e) {
+        if (e.which == 38) {
+			ship.control_acc = true;
+		} else if (e.which == 37) {
+			ship.control_left = true;
+			ship.control_right = false;
+		} else if (e.which == 39) {
+			ship.control_left = false;
+			ship.control_right = true;
+		}
+		$("height").innerHTML = e.which;
+    }
+
+	function onkeyup(e) {
+        if (e.which == 38) {
+			ship.control_acc = false;
+		} else if (e.which == 37 || e.which == 39) {
+			ship.control_left = false;
+			ship.control_right = false;
+		}
+		$("width").innerHTML = e.which;
+    }
+
 	return {
 		init: function(params) {
 			canvas = $("surface");
@@ -53,7 +109,9 @@ var Game = (function() {
 			pmax = new Vector(width, height);
 			ctx = canvas.getContext("2d");
 			ctx.save();	// warum?
-			initBall();
+			initBall(); initSonne(); initShip();
+            attachEvent(window, 'keydown', onkeydown);
+            attachEvent(window, 'keyup', onkeyup);
 			updateCanvas();
 		},
 
@@ -71,7 +129,3 @@ var Game = (function() {
 	}
 	};
 })();
-
-function drawSurface() {
-	
-}
